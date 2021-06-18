6d617422cd8e11ddf9960e2fce0f6a61b7a64667
CheoR <cheo.roman+github@gmail.com>
2021-06-18 14:45:16 -0500

    build: automate loading fixtures on heroku.

---
+++ heroku_load_fixtures.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# To load fixures
+# 
+#  $ heroku run bash heroku_load_fixtures.sh
+#
+######################
+# Data Order Example #
+######################
+
+appName="api"
+pathToFixtures="data/fixtures"
+
+declare -a fixturesArray=(appusers services vessels voyages carriers ports documents dues bkgstatuses cntrstatuses containers products bookings)
+
+
+echo -e '\n============================='
+echo 'Running makemigrations on app'
+echo '============================='
+python3 manage.py makemigrations $appName
+
+
+echo -e '\n==============='
+echo 'Running migrate'
+echo '==============='
+python3 manage.py migrate
+
+
+echo -e '\n================'
+echo 'Loading fixtures'
+echo '================'
+
+for fixture in ${fixturesArray[@]}; do
+   echo -e "\n*************"
+   echo "Loading $fixture"
+   echo "*************"
+   python manage.py loaddata $pathToFixtures/$fixture
+done
+
+echo -e '\n================'
+echo 'DONE'
+echo '================'
