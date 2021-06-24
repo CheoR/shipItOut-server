39e42868e9d5bd87078d266fa518d9f750be008b
CheoR <cheo.roman+github@gmail.com>
2021-06-23 20:42:14 -0500

    build: export data as a glob

--- api/views/bookingViewSet.py
+++ api/views/bookingViewSet.py
@@ -68,6 +68,8 @@ class BookingViewSet(ViewSet):
                 data = {}
 
                 data['id'] = booking.id
+                data['username'] = request.auth.user.username
+                data['full_name'] = F"{request.auth.user.first_name} {request.auth.user.last_name}"
                 data['booking_status'] = status.status
                 data['booking'] = booking.booking
                 data['origin'] = booking.loading_origin
@@ -109,54 +111,63 @@ class BookingViewSet(ViewSet):
             Returns:
                 Response : JSON serialized list of Booking types.
         """
+
+        user = AppUser.objects.get(user=request.auth.user)
+
         try:
-            user = AppUser.objects.get(user=request.auth.user)
             booking = Booking.objects.get(user=user, pk=pk)
-            carrier = Carrier.objects.get(id=booking.carrier.id)
-            status = BkgStatus.objects.get(id=booking.booking_status.id)
-            voyage = Voyage.objects.get(id=booking.voyage_reference.id)
-            vessel = Vessel.objects.get(id=voyage.vessel.id)
-            service = Service.objects.get(id=vessel.service_id)
-            container = Container.objects.get(id=booking.container.id)
-            cntr_status = CntrStatus.objects.get(
-                id=container.container_status.id)
-            port = Port.objects.get(id=booking.port.id)
-            document = Document.objects.get(id=booking.document.id)
-            money = Due.objects.get(id=booking.due.id)
+            # carrier = Carrier.objects.get(id=booking.carrier.id)
+            # status = BkgStatus.objects.get(id=booking.booking_status.id)
+            # voyage = Voyage.objects.get(id=booking.voyage_reference.id)
+            # vessel = Vessel.objects.get(id=voyage.vessel.id)
+            # service = Service.objects.get(id=vessel.service_id)
+            # container = Container.objects.get(id=booking.container.id)
+            # cntr_status = CntrStatus.objects.get(
+            #     id=container.container_status.id)
+            # port = Port.objects.get(id=booking.port.id)
+            # document = Document.objects.get(id=booking.document.id)
+            # money = Due.objects.get(id=booking.due.id)
 
-            data = {}
+            # data = {}
 
-            data['id'] = booking.id
-            data['booking_status'] = status.status
-            data['booking'] = booking.booking
-            data['service'] = service.name
-            data['voyage'] = voyage.voyage
-            data['vessel'] = vessel.name
-            data['longitude'] = vessel.longitude
-            data['latitude'] = vessel.latitude
-            data['carrier'] = carrier.name
-            data['container'] = container.container
-            data['container_status'] = cntr_status.status
-            data['size'] = container.equipment_size
-            data['damaged'] = container.is_damaged
-            data['needs_inspection'] = container.is_need_inspection
-            data['overweight'] = container.is_overweight
-            data['container_notes'] = container.notes
-            data['container_available'] = container.is_in_use
-            data['origin'] = booking.loading_origin
-            data['destination'] = booking.unloading_destination
-            data['pickup_appt'] = booking.pickup_appt
-            data['port'] = port.code
-            data['port_name'] = port.name
-            data['port_location'] = port.location
-            data['port_cutoff'] = booking.port_cutoff
-            data['rail_cutoff'] = booking.rail_cutoff
-            data['document_submitted'] = document.are_docs_ready
-            data['money_owed'] = money.are_dues_paid
-            data['issues'] = booking.has_issue
-            data['notes'] = booking.notes
+            # data['id'] = booking.id
+            # data['username'] = request.auth.user.username
+            # data['full_name'] = F"{request.auth.user.first_name} {request.auth.user.last_name}"
+            # data['booking_status'] = status.status
+            # data['booking'] = booking.booking
+            # data['service'] = service.name
+            # data['voyage'] = voyage.voyage
+            # data['vessel'] = vessel.name
+            # data['longitude'] = vessel.longitude
+            # data['latitude'] = vessel.latitude
+            # data['carrier'] = carrier.name
+            # data['container'] = container.container
+            # data['container_status'] = cntr_status.status
+            # data['size'] = container.equipment_size
+            # data['damaged'] = container.is_damaged
+            # data['needs_inspection'] = container.is_need_inspection
+            # data['overweight'] = container.is_overweight
+            # data['container_notes'] = container.notes
+            # data['container_available'] = container.is_in_use
+            # data['origin'] = booking.loading_origin
+            # data['destination'] = booking.unloading_destination
+            # data['pickup_appt'] = booking.pickup_appt
+            # data['port'] = port.code
+            # data['port_name'] = port.name
+            # data['port_location'] = port.location
+            # data['port_cutoff'] = booking.port_cutoff
+            # data['rail_cutoff'] = booking.rail_cutoff
+            # data['document_submitted'] = document.are_docs_ready
+            # data['money_owed'] = money.are_dues_paid
+            # data['issues'] = booking.has_issue
+            # data['booking_notes'] = booking.notes
 
-            return Response(data)
+            serialzied_bookings = BookingSerializer(
+                booking,
+                context=({'request': request})
+            )
+
+            return Response(serialzied_bookings.data)
         except Booking.DoesNotExist as ex:
             return Resposnse({'message': 'The requested order does not exist, or you do not have permission to access it.'},
                              status=status.HTTP_404_NOT_FOUND
