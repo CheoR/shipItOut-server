bae2fb0d6ad97c82c5023aae82f8f381635ab7a3
CheoR <cheo.roman+github@gmail.com>
2021-06-24 10:40:57 -0500

    feat: user can view products list.

--- api/serializers/productSerializer.py
+++ api/serializers/productSerializer.py
@@ -1,5 +1,6 @@
 """Product Serializer"""
 
+from api.serializers.containerSerializer import ContainerSerializer
 from rest_framework import serializers
 
 from api.models import Product
@@ -8,7 +9,22 @@ from api.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     """JSON serializer for Products"""
 
+    container = ContainerSerializer()
+
     class Meta:
         model = Product
         fields = ('id', 'commodity', 'weight', 'is_fragile', 'is_haz',
                   'is_damaged', 'is_reefer', 'container')
+
+        # depth = 3
+
+    def to_representation(self, obj):
+        """Flatten key,values from one-level deep"""
+        representation = super().to_representation(obj)
+        cs_representation = representation.pop('container')
+        for key in cs_representation:
+            if key == 'id':
+                continue
+            representation[key] = cs_representation[key]
+
+        return representation
