d3b21dfa14a23921125096d8f1f1bcd54fc93751
CheoR <cheo.roman+github@gmail.com>
2022-10-12 07:11:38 -0500

    ref: update server settings

--- server/settings/prod.py
+++ server/settings/prod.py
@@ -2,9 +2,11 @@ from .base import *
 
 DEBUG = False
 # update deploy-this-test to your app name
-ALLOWED_HOSTS = ['shipitout-api.herokuapp.com',
-                 'shipitout.herokuapp.com',
-                 'shipitout.netlify.app']
+ALLOWED_HOSTS = [
+    'shipitout-api.herokuapp.com',
+    'shipitout.herokuapp.com',
+    'shipitout.netlify.app'
+]
 
 CORS_ORIGIN_WHITELIST = (
     'https://shipitout-api.herokuapp.com',
--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,7 @@
 """server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,26 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from rest_framework import routers
 
-from api.views import login_user, register_user
-from api.views import AppUserViewSet
-from api.views import ServiceViewSet
-from api.views import VesselViewSet
-from api.views import VoyageViewSet
-from api.views import CarrierViewSet
-from api.views import PortViewSet
-from api.views import DocumentViewSet
-from api.views import DueViewSet
-from api.views import BkgStatusViewSet
-from api.views import CntrStatusViewSet
-from api.views import ContainerViewSet
-from api.views import ProductViewSet
-from api.views import BookingViewSet
-from api.views import BookingCreateViewSet
+
+from api.views import (
+    register_user,
+    login_user,
+    AppUserViewSet,
+    PortViewSet,
+    VesselViewSet,
+    ContainerViewSet,
+    VoyageViewSet,
+    ProductViewSet,
+    BookingViewSet,
+)
 
 # At any point, your urlpatterns can “include” other URLconf modules.
 #  This essentially “roots” a set of URLs below other ones.
@@ -49,26 +47,20 @@ from api.views import BookingCreateViewSet
 # may no longer need trailing_slash
 router = routers.DefaultRouter(trailing_slash=False)
 
-router.register(r'bookings', BookingViewSet, 'booking')
-router.register(r'appUsers', AppUserViewSet, 'appUser')
-router.register(r'services', ServiceViewSet, 'service')
+router.register(r'ports', PortViewSet, 'port')
 router.register(r'vessels', VesselViewSet, 'vessel')
 router.register(r'voyages', VoyageViewSet, 'voyage')
-router.register(r'carriers', CarrierViewSet, 'carrier')
-router.register(r'ports', PortViewSet, 'port')
-router.register(r'documents', DocumentViewSet, 'document')
-router.register(r'dues', DueViewSet, 'due')
-router.register(r'bkgStatuses', BkgStatusViewSet, 'bkgStatus')
-router.register(r'cntrStatuses', CntrStatusViewSet, 'cntrStatus')
-router.register(r'containers', ContainerViewSet, 'container')
+router.register(r'appusers', AppUserViewSet, 'appUser')
 router.register(r'products', ProductViewSet, 'product')
-router.register(r'create', BookingCreateViewSet, 'create')
+router.register(r'bookings', BookingViewSet, 'booking')
+router.register(r'containers', ContainerViewSet, 'container')
 
 urlpatterns = [
-    path('', include('api.urls', namespace='api')),
-    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),
+    path('', include('api.urls', namespace='api')),
+    path('project-admin/', admin.site.urls),
     path('ship_admin/', admin.site.urls),
 ]
