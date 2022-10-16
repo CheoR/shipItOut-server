627e42b4e8248afd0513eeb98d148778dcb72d48
CheoR <cheo.roman+github@gmail.com>
2022-10-16 10:55:59 -0500

    ref: match up container variable name between front and backend

--- data/ShipItOut2.sql
+++ data/ShipItOut2.sql
@@ -13,7 +13,6 @@ CREATE TABLE "User" (
   "last_login" datetime,
   "date_joined" datetime
 );
-
 CREATE TABLE "AppUser" (
   "id" pk,
   "user_id" int,
@@ -22,7 +21,6 @@ CREATE TABLE "AppUser" (
   "phone" phone,
   "account_type" enum
 );
-
 CREATE TABLE "Booking" (
   "id" int,
   "booking" varchar,
@@ -45,38 +43,26 @@ CREATE TABLE "Booking" (
   "booking_status" enum,
   "booking_notes" longtext
 );
-
 CREATE TABLE "Container" (
   "id" int,
   "container" varchar,
   "booking_id" int,
   "equipment_type" varchar,
-  "equipment_location" enum,
+  "container_location" enum,
   "is_container_damaged" boolean,
   "is_needs_inspection" boolean,
   "is_overweight" boolean,
   "is_in_use" boolean,
   "container_notes" longtext
 );
-
-CREATE TABLE "Port" (
-  "id" pk,
-  "name" varchar,
-  "code" varchar
-);
-
-CREATE TABLE "Vessel" (
-  "id" pk,
-  "name" varchar
-);
-
+CREATE TABLE "Port" ("id" pk, "name" varchar, "code" varchar);
+CREATE TABLE "Vessel" ("id" pk, "name" varchar);
 CREATE TABLE "Voyage" (
   "id" pk,
   "voyage" varchar,
   "vessel_id" int,
   "service" enum
 );
-
 CREATE TABLE "Product" (
   "id" pk,
   "container_id" int,
@@ -88,21 +74,21 @@ CREATE TABLE "Product" (
   "is_haz" boolean,
   "product_notes" longtext
 );
-
-ALTER TABLE "User" ADD FOREIGN KEY ("id") REFERENCES "AppUser" ("user_id");
-
-ALTER TABLE "Booking" ADD FOREIGN KEY ("agent_id") REFERENCES "AppUser" ("id");
-
-ALTER TABLE "Booking" ADD FOREIGN KEY ("carrier_id") REFERENCES "AppUser" ("id");
-
-ALTER TABLE "Booking" ADD FOREIGN KEY ("voyage_id") REFERENCES "Voyage" ("id");
-
-ALTER TABLE "Booking" ADD FOREIGN KEY ("loading_port_id") REFERENCES "Port" ("id");
-
-ALTER TABLE "Booking" ADD FOREIGN KEY ("unloading_port_id") REFERENCES "Port" ("id");
-
-ALTER TABLE "Container" ADD FOREIGN KEY ("booking_id") REFERENCES "Booking" ("id");
-
-ALTER TABLE "Product" ADD FOREIGN KEY ("container_id") REFERENCES "Container" ("id");
-
-ALTER TABLE "Voyage" ADD FOREIGN KEY ("vessel_id") REFERENCES "Vessel" ("id");
+ALTER TABLE "User"
+ADD FOREIGN KEY ("id") REFERENCES "AppUser" ("user_id");
+ALTER TABLE "Booking"
+ADD FOREIGN KEY ("agent_id") REFERENCES "AppUser" ("id");
+ALTER TABLE "Booking"
+ADD FOREIGN KEY ("carrier_id") REFERENCES "AppUser" ("id");
+ALTER TABLE "Booking"
+ADD FOREIGN KEY ("voyage_id") REFERENCES "Voyage" ("id");
+ALTER TABLE "Booking"
+ADD FOREIGN KEY ("loading_port_id") REFERENCES "Port" ("id");
+ALTER TABLE "Booking"
+ADD FOREIGN KEY ("unloading_port_id") REFERENCES "Port" ("id");
+ALTER TABLE "Container"
+ADD FOREIGN KEY ("booking_id") REFERENCES "Booking" ("id");
+ALTER TABLE "Product"
+ADD FOREIGN KEY ("container_id") REFERENCES "Container" ("id");
+ALTER TABLE "Voyage"
+ADD FOREIGN KEY ("vessel_id") REFERENCES "Vessel" ("id");
